e734164751900a1f324cf722254eddabff425f0d
Nick Tanner <tannernc.nt@gmail.com>
2019-01-25 20:25:56 -0500

    Changed file names. Added tests

---
+++ hashtagit.test.js
@@ -0,0 +1,24 @@
+// Need to add test suite
+const hashtagit = require('./');
+
+test('testString should match hashtag', () => {
+  const testString = hashtagit('Are_ we having - fun/ Yet?');
+  const hashtag = '#AreWeHavingFunYet?';
+  expect(testString).toBe(hashtag);
+});
+
+test('should throw an error for too many characters', () => {
+  expect(() => hashtagit('Ni' + 'i'.repeat(143) + 'ce!')).toThrowError(
+    'Must be 140 characters or less'
+  );
+});
+
+test('should throw an error for empty string', () => {
+  expect(() => {
+    hashtagit('');
+  }).toThrowError('Must be a non-empty string');
+});
+
+test('should throw an error for non-string entry', () => {
+  expect(() => hashtagit(4739834)).toThrowError('Must be a non-empty string');
+});
