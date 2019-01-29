78166146bca3fff583d2382f3c6e1278bda481f7
Nick Tanner <tannernc.nt@gmail.com>
2019-01-28 22:35:12 -0500

    Just committed and forgot to remove comments

--- hashtagit.js
+++ hashtagit.js
@@ -1,19 +1,3 @@
-// if (typeof str !== 'string' || str === '') {
-//   throw new Error('Must be a non-empty string');
-// } else if (str.length <= 140) {
-//   const removeDashesandSpaces = str
-//     .replace(/[-_\/]/g, ' ')
-//     .split(' ')
-//     .filter(key => key !== '');
-//   const addHash =
-//     '#' +
-//     removeDashesandSpaces
-//       .map(word => word[0].toUpperCase() + word.substring(1).toLowerCase())
-//       .join('');
-//   return addHash;
-// } else {
-//   throw new Error('Must be 140 characters or less');
-// }
 module.exports = function(str) {
   if (typeof str !== 'string' || str === '') {
     throw new Error('Must be a non-empty string');
