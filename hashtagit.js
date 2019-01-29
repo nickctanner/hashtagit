76e50b839f28855489428f6e36568e8a141882da
Nick Tanner <tannernc.nt@gmail.com>
2019-01-28 22:32:19 -0500

    Refactored and modified tests

--- hashtagit.js
+++ hashtagit.js
@@ -1,17 +1,33 @@
-module.exports = function hashtagit(str) {
+// if (typeof str !== 'string' || str === '') {
+//   throw new Error('Must be a non-empty string');
+// } else if (str.length <= 140) {
+//   const removeDashesandSpaces = str
+//     .replace(/[-_\/]/g, ' ')
+//     .split(' ')
+//     .filter(key => key !== '');
+//   const addHash =
+//     '#' +
+//     removeDashesandSpaces
+//       .map(word => word[0].toUpperCase() + word.substring(1).toLowerCase())
+//       .join('');
+//   return addHash;
+// } else {
+//   throw new Error('Must be 140 characters or less');
+// }
+module.exports = function(str) {
   if (typeof str !== 'string' || str === '') {
     throw new Error('Must be a non-empty string');
   } else if (str.length <= 140) {
-    const removeDashesandSpaces = str
-      .replace(/[-_\/]/g, ' ')
-      .split(' ')
-      .filter(key => key !== '');
-    const addHash =
+    return (
       '#' +
-      removeDashesandSpaces
-        .map(word => word[0].toUpperCase() + word.substring(1).toLowerCase())
-        .join('');
-    return addHash;
+      str
+        .replace(/[-_\/.]/g, ' ')
+        .replace(
+          /\w\S*/g,
+          word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        )
+        .replace(/\s/g, '')
+    );
   } else {
     throw new Error('Must be 140 characters or less');
   }
--- hashtagit.test.js
+++ hashtagit.test.js
@@ -2,13 +2,13 @@
 const hashtagit = require('./');
 
 test('testString should match hashtag', () => {
-  const testString = hashtagit('Are_ we having - fun/ Yet?');
+  const testString = hashtagit('Are_ we having - fun/ ...Yet?');
   const hashtag = '#AreWeHavingFunYet?';
   expect(testString).toBe(hashtag);
 });
 
 test('should throw an error for too many characters', () => {
-  expect(() => hashtagit('Ni' + 'i'.repeat(143) + 'ce!')).toThrowError(
+  expect(() => hashtagit('N' + 'i'.repeat(143) + 'ce!')).toThrowError(
     'Must be 140 characters or less'
   );
 });
